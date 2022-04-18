9027667ec566b8bb86692154c1f1e5773fe067fd
C J Silverio <ceejceej@gmail.com>
2022-04-17 18:14:47 -0700

    Fix Slack authentication in blocking client
    The Slack API has moved on, while this crate has not. Adapted
    the approach from https://github.com/slack-rs/slack-rs-api/issues/107
    to pluck the auth token out of the parameter body and send it as
    a bearer auth header instead. It is rather surprising to realize that
    Slack ever accepted this as a body param, but hey.

--- src/sync/requests.rs
+++ src/sync/requests.rs
@@ -43,9 +43,25 @@ mod reqwest_support {
         {
             let mut url = reqwest::Url::parse(method_url.as_ref()).expect("Unable to parse url");
 
-            url.query_pairs_mut().extend_pairs(params);
+            let mut token = None;
+            {
+                let mut qp = url.query_pairs_mut();
+                for param in params {
+                    let (k, v) = param.borrow();
+                    if k.as_ref() == "token" {
+                        token = Some(v.as_ref().to_owned());
+                        continue;
+                    }
+                    qp.append_pair(k.as_ref(), v.as_ref());
+                }
+            }
 
-            Ok(self.get(url).send()?.text()?)
+            let mut req = self.get(url);
+            if let Some(token) = token {
+                req = req.bearer_auth(token);
+            };
+
+            Ok(req.send()?.text()?)
         }
     }
 
